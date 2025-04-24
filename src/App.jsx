7033dc4bb82fef6f8c2f233cd523447dba3aa7cf
Vitória Linhares <vitorialinhares2023@gmail.com>
2025-04-24 08:09:53 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import imgAnimada from "../public/animada.svg"
 
-export default function App(){
-    return (
-      <div className="w-full h-screen flex bg-[#201B2C]">
-        <div className="W-[50%] h-full flex items-center justify-center flex-col">
-            <h1 className="text-[30px] font-bold text-[#008cff]">Entre para o nosso time</h1>
-            <img src={imgAnimada} alt="imagem-anima" width={500}/>
-        </div>
-        <div className="w-[50%] h-full items-center justify-center">  
-            <div className="w-[50%] h-[50px] bg-[#2F2942] rounded-xl"></div>
-                <div className="w-full h-[5%] bg-amber-50"></div>
-                <h1 className="text-[20px] text-[#8466e9] font-bold">LOGIN </h1>
-                <div className="w-full h-[70%] bg-gray-800">
-                  <label htmlFor="user" className="text-white">Usuário</label>
-                  <input type="text" id="user" className="w-full rounded-md p-[20px] bg-[#514768] text-white m-4" />
-                  <input type="text" />
-                </div>
-                <div className="w-full h-[10%] bg-gray-800"></div>
-s        </div>
+export default function App() {
+  return (
+    <div className="w-full h-screen flex bg-[#201B2C]">
+      <div className="W-[50%] h-full flex items-center justify-center flex-col">
+        <h1 className="text-[30px] font-bold text-[#008cff]">Entre para o nosso time</h1>
+        <img src={imgAnimada} alt="imagem-anima" width={500} />
       </div>
-      
-    )
+      <div className="w-[50%] h-full flex items-center justify-center">
+        <div className="w-[50%] h-[350px] bg-[#2F2942] rounded-xl">
+          <div className="w-full h-[15%] flex items-center justify-center">
+            <h1 className="text-[20px] text-[#8466e9] font-bold">LOGIN </h1>
+          </div>
+          <div className="w-full h-[70%] p-[20px]">
+            <label htmlFor="user" className="text-white">Usuário</label>
+            <input type="text" id="user" className="w-full rounded-md p-[10px] bg-[#514768] text-white mb-4" />
+            <label htmlFor="pass" className="text-white">Senha</label>
+            <input type="password" id="pass" className="w-full rounded-md bg-[#514768] p-[10px] text-white" />
+          </div>
+          <div className="w-full h-[10%]">
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  )
 }
